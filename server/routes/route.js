342a4b5986664ac3c08a06664420241e22d7cde4
Janindu Sachinthana <106908984+Janindu98@users.noreply.github.com>
2022-11-02 16:18:06 +0530

    Create route.js

---
+++ server/routes/route.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+
+const Route = require('./../models/route');
+
+router.post('/create', async (req, res) => {
+    const OtherPoints = [];
+    OtherPoints.push(req.body.OtherPoints);
+
+    const nRoute = await new Route({
+        RouteNumber:req.body.RouteNumber,
+        StartPoint:req.body.StartPoint,
+        Endpoint:req.body.Endpoint,
+        OtherPoints:OtherPoints
+    }).save()
+    .then(() => res.json("New route created successfully.."))
+    .catch((err) => res.json(err.message));
+});
+
+router.put('/edit/:id', async (req, res) => {
+    const OtherPoints = [];
+    OtherPoints.push(req.body.OtherPoints);
+
+    const obj = await Route.findById(req.params.id);    
+
+    if(obj) {
+        obj.RouteNumber = req.body.RouteNumber;
+        obj.StartPoint = req.body.StartPoint;
+        obj.Endpoint = req.body.EndPoint;
+        obj.OtherPoints = OtherPoints;
+
+        await obj.save()
+        .then(() => res.json('Route details updated successfully.'))
+        .catch((err) => res.json(err.message));
+    } else {
+        res.status(123).json('Invalid ID');
+    }
+});
+
+router.get('/all', (req, res) => {
+    Route.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err.message));
+});
+
+router.get('/:id', (req, res) => {
+    Route.findById(req.params.id)
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err.message));
+});
+
+router.delete('/point/:id/:point', async (req, res) => {
+    const obj = await Route.findById(req.params.id);
+
+    if(obj) {
+        obj.OtherPoints.splice(obj.OtherPoints.findIndex((Object) => Object.pointName == req.params.point), 1);
+
+        await obj.save()
+        .then(() => res.json('Point removed successfully'))
+        .catch((err) => res.json(err.message));
+    } else {
+        res.status(123).json("No point details Found!"); 
+    }
+});
+
+router.delete('/remove/:id', (req, res) =>{
+    Route.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Route Deleted"))
+    .catch((err) => res.json(err.message))
+});
+
+module.exports = router;
