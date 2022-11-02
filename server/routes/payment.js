36ba3090e9793963b7c2d661a3511d7e47759279
Janindu Sachinthana <106908984+Janindu98@users.noreply.github.com>
2022-11-02 21:36:51 +0530

    Create payment.js

---
+++ server/routes/payment.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const Payment = require('./../models/payment');
+
+router.post('/create', async (req, res) => {
+    const payment = new Payment({
+        UserID:req.body.UserID,
+        Date:req.body.Date,
+        Time:req.body.Time,
+        Type:req.body.Type
+    }).save()
+    .then(() => res.json("New payment added successfully.."))
+    .catch((err) => res.json(err.message)); 
+});
+
+router.get('/:UserID', (req, res) => {
+    Payment.findOne({UserID:req.params.UserID})
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err.message));
+});
+
+router.get('/all', (req, res) => {
+    Payment.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err.message));
+});
+
+module.exports = router;
